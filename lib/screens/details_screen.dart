887625c37754d834aa85e02d5e80e0e27434a1da
LuisMeraz01227 <luis_mbs05@hotmail.com>
2024-02-15 09:47:34 -0700

    primer commit

---
+++ lib/screens/details_screen.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('Details screen'),
+    );
+  }
+}
