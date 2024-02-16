863ec4aa159634d63003a3b68560890cdd6866f7
LuisMeraz01227 <luis_mbs05@hotmail.com>
2024-02-15 20:25:32 -0700

    Primeras pruebas card swiper

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,25 @@
 
 import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/widgets.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        
-        title: const Text('Home'),
+    return Center(
+      child: Scaffold(
+        appBar: AppBar(
+          
+          title: const Text('Home'),
+        ),
+        body: const Center(
+          child: CardMovie(
+          
+          )
+        ),
       ),
-      body: const Text('Home Screen'),
     );
   }
 }
--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,1 +1,20 @@
 
+import 'package:card_swiper/card_swiper.dart';
+import 'package:flutter/material.dart';
+
+class CardMovie extends StatelessWidget {
+  const CardMovie({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Swiper(
+        itemBuilder: (BuildContext context,int index){
+          return Image.network("https://via.placeholder.com/350x150",fit: BoxFit.fill,);
+        },
+        itemCount: 3,
+        pagination: const SwiperPagination(),
+        control: const SwiperControl(),
+        
+      );
+  }
+}
