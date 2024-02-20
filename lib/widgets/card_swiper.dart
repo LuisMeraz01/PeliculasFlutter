da6f590d6cd932e9681ae54d7d6916343e997c69
LuisMeraz01227 <luis_mbs05@hotmail.com>
2024-02-19 18:26:04 -0700

    card y movie slider

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -7,15 +7,31 @@ class CardMovie extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Swiper(
-        itemBuilder: (BuildContext context,int index){
-          return Container(
-            color: const Color.fromARGB(255, 0, 0, 0),
+
+    final size = MediaQuery.of(context).size;
+
+    return SizedBox(
+
+      width: double.infinity,
+      height: size.height * 0.5,
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK,
+        itemWidth: size.width * 0.6,
+        itemHeight: size.height * 0.4,
+        itemBuilder: (_, int index) {
+          return GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'details', arguments: ''),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                placeholder: AssetImage('assetName'),
+                image: AssetImage('assetName'),
+              ),
+            ),
           );
         },
-        itemCount: 3,
-        pagination: const SwiperPagination(),
-        control: const SwiperControl(),
-      );
+      ),
+    );
   }
 }
